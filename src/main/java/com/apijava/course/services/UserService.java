5bfac6ad83bd9cdcdccd188ca58f0b1501843394
Gabriel Santos <gabrielsantos98898@gmail.com>
2023-02-09 14:03:13 -0300

    Colocado exception para a pesquisa de users

--- src/main/java/com/apijava/course/services/UserService.java
+++ src/main/java/com/apijava/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.apijava.course.entities.User;
 import com.apijava.course.repositories.UserRepository;
+import com.apijava.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj =  repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
